f26a54341ae6afbcb3150f5fb9406431dd994a71
rls21 <robin2103-leon@hotmail.com>
2023-11-25 15:09:17 -0500

    Buscar2

--- WinApp_Biblioteca/Buscar.Designer.cs
+++ WinApp_Biblioteca/Buscar.Designer.cs
@@ -31,7 +31,14 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cAutor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cEdición = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cEditorial = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -54,23 +61,72 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(25, 198);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.cCodigo,
+            this.cNombre,
+            this.cAutor,
+            this.cEdición,
+            this.cEditorial});
+            this.dataGridView1.Location = new System.Drawing.Point(71, 187);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(539, 90);
+            this.dataGridView1.Size = new System.Drawing.Size(473, 90);
             this.dataGridView1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WinApp_Biblioteca.Properties.Resources.buscar;
+            this.pictureBox1.Location = new System.Drawing.Point(270, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(55, 43);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cCodigo
+            // 
+            this.cCodigo.HeaderText = "Código";
+            this.cCodigo.Name = "cCodigo";
+            this.cCodigo.Width = 60;
+            // 
+            // cNombre
+            // 
+            this.cNombre.HeaderText = "Nombre";
+            this.cNombre.Name = "cNombre";
+            this.cNombre.Width = 60;
+            // 
+            // cAutor
+            // 
+            this.cAutor.HeaderText = "Autor";
+            this.cAutor.Name = "cAutor";
+            this.cAutor.Width = 60;
+            // 
+            // cEdición
+            // 
+            this.cEdición.HeaderText = "Column1";
+            this.cEdición.Name = "cEdición";
+            this.cEdición.Width = 60;
+            // 
+            // cEditorial
+            // 
+            this.cEditorial.HeaderText = "Editorial";
+            this.cEditorial.Name = "cEditorial";
+            this.cEditorial.Width = 60;
             // 
             // Buscar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(592, 300);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Buscar";
             this.Text = "Buscar";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,5 +137,11 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cCodigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cAutor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cEdición;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cEditorial;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
