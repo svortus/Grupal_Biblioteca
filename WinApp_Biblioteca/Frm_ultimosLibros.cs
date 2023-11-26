b175433cf472d4c093a6e3193ade54469925240a
PabloGitHub25 <ldupablo21@gmail.com>
2023-11-26 13:11:05 -0500

    .

--- WinApp_Biblioteca/Frm_ultimosLibros.cs
+++ WinApp_Biblioteca/Frm_ultimosLibros.cs
@@ -17,17 +17,18 @@ namespace WinApp_Biblioteca
             InitializeComponent();
 
             // Obtener el índice del primer elemento a copiar
-            int indiceInicio = Math.Max(0, NuevoDgw.Rows.Count - 6);
+            int indiceInicio = Math.Max(0, NuevoDgw.Rows.Count - 5);
 
             if (NuevoDgw != null)
             {
+                // Limpiar Dgw4 antes de agregar nuevas filas
+                Dgw4.Rows.Clear();
+
                 // Iterar solo sobre las últimas 5 filas del DataGridView original
                 for (int i = indiceInicio; i < NuevoDgw.Rows.Count; i++)
                 {
-                    // Obtener el índice de la nueva fila en Dgw4
+                    // Agregar nueva fila a Dgw4
                     int nuevoIndice = Dgw4.Rows.Add();
-
-                    // Copiar los valores de las celdas del DataGridView original a Dgw4
                     Dgw4.Rows[nuevoIndice].Cells["Código"].Value = NuevoDgw.Rows[i].Cells[0].Value;
                     Dgw4.Rows[nuevoIndice].Cells["Nombre"].Value = NuevoDgw.Rows[i].Cells[1].Value;
                     Dgw4.Rows[nuevoIndice].Cells["Autor"].Value = NuevoDgw.Rows[i].Cells[2].Value;
