7afc664df0f170932095d5a0382a3d46da927081
svortus <jhosgonza24@gmail.com>
2023-11-26 13:02:15 -0500

    xd

--- WinApp_Biblioteca/Form1.cs
+++ WinApp_Biblioteca/Form1.cs
@@ -66,7 +66,7 @@ namespace WinApp_Biblioteca
             {
                 
                 Frm_Menu objFM = new Frm_Menu(n);
-                objFM.ShowDialog();
+                objFM.ShowDialog();//c
                 Close();
             }
             else
